b8cb523e72f8f84f2806114289c92c067f1ef029
Shaodi Sun <shaodi.sun@mail.mcgill.ca>
2019-10-14 15:31:29 -0400

    Added functionality to retrieve prices with terms non-headlessly

--- main.rb
+++ main.rb
@@ -6,11 +6,11 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'pp'
-
+require 'selenium-webdriver'
 
 puts "Welcome to statring price CLI"
 prompt = TTY::Prompt.new
-option = prompt.yes?("Do you want to retrieve the names of the top 12 devices from Bell Smartphone page? \nType y/Y to continue, n to exit")
+option = prompt.yes?("Do you want to retrieve the names of the top 12 devices from Bell Smartphone page?")
   if option == true
     PAGE_URL = 'https://www.bell.ca/Mobility/Smartphones_and_mobile_internet_devices'
     document = Nokogiri::HTML(open(PAGE_URL).read)
@@ -19,5 +19,21 @@ option = prompt.yes?("Do you want to retrieve the names of the top 12 devices fr
     end
     top12Products = fullProductsList[0..11]
   end
-  # puts top12Products
-  prompt.select("Choose the device to get more info", top12Products)
+
+chosenProduct = prompt.select("Choose the device to get more info", top12Products, filter: true) 
+productIndex = top12Products.index(chosenProduct)
+class ProductDetails< Struct.new(:name, :prices, :terms)
+end
+
+
+driver = Selenium::WebDriver.for :chrome
+driver.get 'https://www.bell.ca/Mobility/Smartphones_and_mobile_internet_devices'
+overflowEl = driver.find_elements(:css, 'div.rsx-product-list-product-details-col')[0..11]
+overflowEl[productIndex.to_i].find_element(:class, 'rsx-product-hotspot').click()
+smartPay = driver.find_elements(:class, 'rsx-radio')[0].click()
+puts("start of smart pay")
+puts driver.find_element(:id,'bcx-order-now-group-smartpay').text
+smartPay = driver.find_elements(:class, 'rsx-radio')[1].click()
+puts("start of subsidized")
+puts driver.find_element(:id,'bcx-order-now-group-subsidized').text
+driver.quit()
