32e2e20cf65a60b663e6fbbbd41148ba4fd6f675
Shaodi Sun <shaodi.sun@mail.mcgill.ca>
2019-10-16 13:56:06 -0400

    format the response to be more readable

--- main.rb
+++ main.rb
@@ -1,14 +1,12 @@
 require "tty-prompt"
-require 'net/http'
 require 'json'
 require "httparty"
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-require 'pp'
 require 'selenium-webdriver'
 
-puts "Welcome to statring price CLI"
+puts "Welcome to Bell statring price CLI"
 prompt = TTY::Prompt.new
 option = prompt.yes?("Do you want to retrieve the names of the top 12 devices from Bell Smartphone page?")
   if option == true
@@ -18,6 +16,8 @@ option = prompt.yes?("Do you want to retrieve the names of the top 12 devices fr
       date = product.at_css('.rsx-product-name-group.rsx-product-list-product-name-top.rsx-h3').at_css('.rsx-product-name.hidden-xs.hidden-sm').text.strip
     end
     top12Products = fullProductsList[0..11]
+  else 
+    exit
   end
 
 chosenProduct = prompt.select("Choose the device to get more info", top12Products, filter: true) 
@@ -30,10 +30,19 @@ driver = Selenium::WebDriver.for :chrome
 driver.get 'https://www.bell.ca/Mobility/Smartphones_and_mobile_internet_devices'
 overflowEl = driver.find_elements(:css, 'div.rsx-product-list-product-details-col')[0..11]
 overflowEl[productIndex.to_i].find_element(:class, 'rsx-product-hotspot').click()
-smartPay = driver.find_elements(:class, 'rsx-radio')[0].click()
-puts("start of smart pay")
-puts driver.find_element(:id,'bcx-order-now-group-smartpay').text
-smartPay = driver.find_elements(:class, 'rsx-radio')[1].click()
-puts("start of subsidized")
-puts driver.find_element(:id,'bcx-order-now-group-subsidized').text
+payment = driver.find_elements(:class, 'rsx-radio')[0].click()
+smartPayIndex = 1
+puts("Smart Payment Options: ")
+driver.find_element(:id,'bcx-order-now-group-smartpay').find_elements(:class, 'payments').map do |payments|
+  puts smartPayIndex.to_s+": "+payments.text
+  smartPayIndex+=1
+end
+puts
+payment = driver.find_elements(:class, 'rsx-radio')[1].click()
+subsidizedPayIndex = 1
+puts("Subsidized Payment Options: ")
+driver.find_element(:id,'bcx-order-now-group-subsidized').find_elements(:class, 'bcx-order-now-box-body').map do |payments|
+    puts subsidizedPayIndex.to_s+": "+payments.text
+    subsidizedPayIndex+=1
+end
 driver.quit()
